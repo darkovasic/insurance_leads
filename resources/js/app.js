3503f03dba24b315ac17e983d39a95df0d779e5a
Darko Vasic <darko.vasic@extraordinary.rs>
2020-01-26 03:14:05 +0100

    vuex update

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,8 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import store from './store/index'
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -19,7 +21,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('agent-dashboard', require('./components/AgentDashboard.vue').default);
 
 /**
@@ -30,4 +32,5 @@ Vue.component('agent-dashboard', require('./components/AgentDashboard.vue').defa
 
 const app = new Vue({
     el: '#app',
+    store
 });
