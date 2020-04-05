60f12b3df087a059694146b047b58d2cc5e3726a
Nikola Dukic <dukic.n@gmail.com>
2020-04-05 13:01:17 +0200

    Moving clear-cache route code to own controller.

---
+++ app/Http/Controllers/ClearCacheController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Artisan;
+use Symfony\Component\Console\Output\StreamOutput;
+
+class ClearCacheController extends Controller
+{
+    /**
+     * Artisan commands to be run
+     *
+     * @var array
+     */
+    protected $commands = [
+        // 'optimize:clear', // One command to clear them all?
+        'cache:clear',
+        'config:cache',
+        'view:cache',
+        'route:clear'
+        // 'route:cache', // Ne funkcionise zbog 'closure' u rutama
+    ];
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $command_output = '';
+        foreach ($this->commands as $command)
+        {
+            $stream = fopen("php://output", "w");
+            Artisan::call($command, array(), new StreamOutput($stream));
+            file_put_contents('php://output', '<br>');
+        }
+
+        file_put_contents('php://output', '<br>');
+        return 'DONE!';
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -32,19 +32,4 @@ Route::get('send_test_email', function(){
 	});
 });
 
-Route::get('/clear-cache', function() {
-    $exitCode = Artisan::call('config:clear');
-    $exitCode = Artisan::call('cache:clear');
-    $exitCode = Artisan::call('config:cache');
-
-    $return =
-        "url: '" . env('DATABASE_URL') . "'<br>" .
-        "host: '" . env('DB_HOST') . "'<br>" .
-        "port: '" . env('DB_PORT') . "'<br>" .
-        "database: '" . env('DB_DATABASE') . "'<br>" .
-        "username: '" . env('DB_USERNAME') . "'<br>" .
-        "password: '" . env('DB_PASSWORD') . "'<br>" .
-        "<br>DONE<br>";
-
-    return 'DONE'; //Return anything
-});
+Route::get('/clear-cache', 'ClearCacheController@index')->middleware('can:register_user');
