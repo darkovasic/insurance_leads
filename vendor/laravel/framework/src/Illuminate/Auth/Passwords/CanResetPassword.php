b5ac3ed8ca762423920dbd6a13c7dfdb06b2e276
Nikola Dukic <dukic.n@gmail.com>
2020-06-10 12:14:45 +0200

    Pasword reset email 01

--- vendor/laravel/framework/src/Illuminate/Auth/Passwords/CanResetPassword.php
+++ vendor/laravel/framework/src/Illuminate/Auth/Passwords/CanResetPassword.php
@@ -24,32 +24,6 @@ trait CanResetPassword
      */
     public function sendPasswordResetNotification($token)
     {
-        $message = "
-            <html>
-            <head>
-            <title>Password reset</title>
-            </head>
-            <body>
-            <p>Hi!</p>
-            <br>
-            <p>We received a request to reset your " . env('APP_NAME') . " password.</p>
-            <br>
-            <p>If you’re having trouble clicking the <a href='" . env('APP_URL') . "password/reset/$token'>Password reset</a> link, copy and paste the URL below.<br></p>
-            <p>" . env('APP_URL') . "password/reset/$token</p>
-            </body>
-            </html>
-        ";
-
-        // Always set content-type when sending HTML email
-        $headers = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-        // More headers
-        $headers .= 'From: ' . env('MAIL_FROM_NAME') . ' <' . env('MAIL_FROM_ADDRESS') . '>' . "\r\n";
-
-        mail($this->email, "Password reset", $message, $headers);
-
-        // Ovde bi u stvari trebalo posalti email sa linkom za reset
-        // $this->notify(new ResetPasswordNotification($token));
+        $this->notify(new ResetPasswordNotification($token));
     }
 }
